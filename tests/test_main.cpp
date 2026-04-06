1ab9672eb364ca6bdd2ece7018890a619b66eb27
Felix Huang <akafelixab@gmail.com>
2026-04-05 21:01:28 -0400

    :bug: fix[tests]: streamline test_main.cpp by removing unused CPU instance and reordering includes

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,26 +1,19 @@
-// test_main.cpp
-// Example unit test using Catch2 (if enabled)
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
 
-#include "../src/utils/Disassembler.h"
-#include "../src/core/RP2A03.h"
 #include "../src/core/Bus.h"
+#include "../src/core/RP2A03.h"
+#include "../src/utils/Disassembler.h"
 
 TEST_CASE("Basic test", "[utils]") {
     // Create a bus and CPU instance for testing
     Bus bus;
-    RP2A03 cpu;
-
-    // Connect the CPU to the bus
-    cpu.ConnectBus(&bus);
 
     // Write some test data to the bus
     bus.write(0x0000, 0xA9); // LDA Immediate opcode
     bus.write(0x0001, 0x01); // Operand for LDA
 
     // Create a disassembler instance
-    Disassembler disasm(&bus, cpu.getLookup());
+    Disassembler disasm(&bus, bus.cpu.getLookup());
 
     // Disassemble the instruction at address 0x0000
     auto line = disasm.disassemble(0x0000);
